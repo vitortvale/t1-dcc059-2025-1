99efef9d1d24f1ea4223c28cc0b96d5af5245fc2
Davi Gomes <odavi4277@gmail.com>
2025-06-25 16:42:53 -0300

    Update Aresta.h

--- src/Aresta.h
+++ src/Aresta.h
@@ -1,18 +1,27 @@
 #ifndef ARESTA_H
 #define ARESTA_H
 
-using namespace std;
+// Forward declaration para evitar inclusão circular e facilitar dependências
+class No; // Declara que 'No' é uma classe, para podermos usar 'No*'
+
 class Aresta {
 public:
-    Aresta(char id_no_alvo);
-    Aresta(char id_no_alvo, int peso);
+    // Construtor para arestas não ponderadas (peso padrão 0)
+    // O ponteiro para o 'No' de destino é passado aqui
+    Aresta(No* no_alvo_ptr);
+
+    // Construtor para arestas ponderadas
+    Aresta(No* no_alvo_ptr, int peso);
+
     ~Aresta() = default;
 
-    char id_no_alvo;
-    int peso;
-    
+    // Métodos "getters" para acessar os dados (boa prática de encapsulamento)
+    No* getNoAlvo() const { return no_alvo_ptr; }
+    int getPeso() const { return peso; }
+
+private:
+    No* no_alvo_ptr; // Ponteiro para o objeto No que é o alvo desta aresta
+    int peso;        // Peso da aresta (0 para não ponderadas, ou um valor específico)
 };
 
-
-
-#endif //ARESTA_H
+#endif // ARESTA_H
