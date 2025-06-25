1e0b87ecb7e6945e73e5b53499cdd8d87c7a1d3b
Davi Gomes <odavi4277@gmail.com>
2025-06-25 16:41:17 -0300

    Update No.cpp

--- src/No.cpp
+++ src/No.cpp
@@ -1,14 +1,12 @@
 #include "No.h"
 
-No::No(char id, int peso) {
-    this->id = id;
-    this->peso = peso;
+No::No(char id, int peso) : id(id), peso_vertice(peso), indice_interno(-1) {
+    // -1 indica que o índice ainda não foi definido pelo Grafo
 }
 
-No::No(char id) {
-    this->id = id;
+No::No(char id) : id(id), peso_vertice(0), indice_interno(-1) {
+    // Peso padrão 0 para nós não ponderados
 }
-
 
 void No::add_aresta(Aresta *a) {
     this->arestas.push_back(a);
